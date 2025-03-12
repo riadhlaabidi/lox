555bfceec237bcf412588f60931eb2c68a2fa637
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-03-12 21:57:14 +0100

    Implement visitor pattern for expressions

--- src/main/java/tech/riadh/lox/Expr.java
+++ src/main/java/tech/riadh/lox/Expr.java
@@ -1,6 +1,19 @@
 package tech.riadh.lox;
 
 abstract class Expr {
+
+	interface Visitor<R> {
+		R visitBinaryExpr(Binary expr);
+
+		R visitGroupingExpr(Grouping expr);
+
+		R visitLiteralExpr(Literal expr);
+
+		R visitUnaryExpr(Unary expr);
+	}
+
+	abstract <R> R accept(Visitor<R> visitor);
+
 	static class Binary extends Expr {
 		final Expr left;
 		final Token operator;
@@ -11,6 +24,11 @@ abstract class Expr {
 			this.operator = operator;
 			this.right = right;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBinaryExpr(this);
+		}
 	}
 
 	static class Grouping extends Expr {
@@ -19,6 +37,11 @@ abstract class Expr {
 		Grouping(Expr expression) {
 			this.expression = expression;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitGroupingExpr(this);
+		}
 	}
 
 	static class Literal extends Expr {
@@ -26,6 +49,11 @@ abstract class Expr {
 
 		Literal(Object value) {
 			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitLiteralExpr(this);
 		}
 	}
 
@@ -36,6 +64,11 @@ abstract class Expr {
 		Unary(Token operator, Expr right) {
 			this.operator = operator;
 			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitUnaryExpr(this);
 		}
 	}
 }
