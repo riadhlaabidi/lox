bf71548e29fee0b34368c24e5e730d6e71bbda70
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-03-14 20:47:40 +0100

    RPN expression printer challenge

---
+++ src/main/java/tech/riadh/lox/RpnPrinter.java
@@ -0,0 +1,65 @@
+package tech.riadh.lox;
+
+import tech.riadh.lox.Expr.Binary;
+import tech.riadh.lox.Expr.Grouping;
+import tech.riadh.lox.Expr.Literal;
+import tech.riadh.lox.Expr.Unary;
+
+/**
+ * NOTE: Chapter 5: Representing code -> Challenges -> 2
+ *
+ * A visitor class that converts an expression to
+ * Reverse Polish Notation (both operands places before the operator)
+ */
+class RpnPrinter implements Expr.Visitor<String> {
+
+	@Override
+	public String visitBinaryExpr(Binary expr) {
+		return expr.left.accept(this) + " "
+				+ expr.right.accept(this) + " "
+				+ expr.operator.lexeme;
+	}
+
+	@Override
+	public String visitGroupingExpr(Grouping expr) {
+		return expr.expression.accept(this);
+	}
+
+	@Override
+	public String visitLiteralExpr(Literal expr) {
+		if (expr.value == null) {
+			return "nil";
+		}
+		return expr.value.toString();
+	}
+
+	@Override
+	public String visitUnaryExpr(Unary expr) {
+		return expr.right.accept(this) + " " + expr.operator.lexeme;
+	}
+
+	private String print(Expr expr) {
+		return expr.accept(this);
+	}
+
+	public static void main(String[] args) {
+		// (1 + 2) * (4 - 3)
+		Expr exp = new Expr.Binary(
+				// (1 + 2)
+				new Expr.Grouping(
+						new Expr.Binary(
+								new Expr.Literal(1),
+								new Token(TokenType.PLUS, "+", null, 1),
+								new Expr.Literal(2))),
+				// *
+				new Token(TokenType.STAR, "*", null, 1),
+				// (4 - 3)
+				new Expr.Grouping(
+						new Expr.Binary(
+								new Expr.Literal(4),
+								new Token(TokenType.MINUS, "-", null, 1),
+								new Expr.Literal(3))));
+
+		System.out.println(new RpnPrinter().print(exp));
+	}
+}
