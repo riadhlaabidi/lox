fcf922286066ea857adfc2e299779fa11f753421
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-04-01 20:19:48 +0100

    Local functions & closures

--- src/main/java/tech/riadh/lox/Interpreter.java
+++ src/main/java/tech/riadh/lox/Interpreter.java
@@ -217,7 +217,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitFunctionStatement(Function stmt) {
-		LoxFunction function = new LoxFunction(stmt);
+		LoxFunction function = new LoxFunction(stmt, environment);
 		environment.define(stmt.name.lexeme, function);
 		return null;
 	}
--- src/main/java/tech/riadh/lox/LoxFunction.java
+++ src/main/java/tech/riadh/lox/LoxFunction.java
@@ -6,9 +6,11 @@ import tech.riadh.lox.Stmt.Function;
 
 class LoxFunction implements LoxCallable {
 	private final Stmt.Function declaration;
+	private final Environment closure;
 
-	LoxFunction(Function declaration) {
+	LoxFunction(Function declaration, Environment closure) {
 		this.declaration = declaration;
+		this.closure = closure;
 	}
 
 	@Override
@@ -18,7 +20,7 @@ class LoxFunction implements LoxCallable {
 
 	@Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
-		Environment env = new Environment(interpreter.globals);
+		Environment env = new Environment(closure);
 
 		for (int i = 0; i < declaration.params.size(); i++) {
 			env.define(declaration.params.get(i).lexeme, arguments.get(i));
