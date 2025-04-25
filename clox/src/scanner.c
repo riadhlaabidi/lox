e2c963503a3c243dbf6000a81ba479964c1086cd
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-04-25 20:14:24 +0100

    Scan identifiers and reserved keywords

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -1,8 +1,10 @@
-#include "scanner.h"
 #include <string.h>
+
+#include "scanner.h"
 
 static int is_at_end(Scanner *scanner);
 static int is_digit(char c);
+static int is_alpha(char c);
 static Token make_token(Scanner *scanner, TokenType type);
 static Token error_token(Scanner *scanner, const char *msg);
 static char advance(Scanner *scanner);
@@ -12,6 +14,10 @@ static char peek(Scanner *scanner);
 static char peek_next(Scanner *scanner);
 static Token string(Scanner *scanner);
 static Token number(Scanner *scanner);
+static Token identifier(Scanner *scanner);
+static TokenType identifier_type(Scanner *scanner);
+static TokenType check_keyword(Scanner *scanner, int start, int length,
+                               const char *rest, TokenType type);
 
 void init_scanner(Scanner *scanner, const char *source)
 {
@@ -31,6 +37,8 @@ Token scan_token(Scanner *scanner)
 
     char c = advance(scanner);
 
+    if (is_alpha(c))
+        return identifier(scanner);
     if (is_digit(c))
         return number(scanner);
 
@@ -87,6 +95,11 @@ Token scan_token(Scanner *scanner)
 static int is_at_end(Scanner *scanner) { return *scanner->current == '\0'; }
 
 static int is_digit(char c) { return c >= '0' && c <= '9'; }
+
+static int is_alpha(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
 
 static char advance(Scanner *scanner)
 {
@@ -158,6 +171,77 @@ static Token number(Scanner *scanner)
     }
 
     return make_token(scanner, TOKEN_NUMBER);
+}
+
+static Token identifier(Scanner *scanner)
+{
+    while (is_alpha(peek(scanner)) || is_digit(peek(scanner))) {
+        advance(scanner);
+    }
+
+    return make_token(scanner, identifier_type(scanner));
+}
+
+static TokenType identifier_type(Scanner *scanner)
+{
+    switch (scanner->start[0]) {
+        case 'a':
+            return check_keyword(scanner, 1, 2, "nd", TOKEN_AND);
+        case 'c':
+            return check_keyword(scanner, 1, 4, "lass", TOKEN_CLASS);
+        case 'e':
+            return check_keyword(scanner, 1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if (scanner->current - scanner->start > 1) {
+                switch (scanner->start[1]) {
+                    case 'a':
+                        return check_keyword(scanner, 2, 3, "lse", TOKEN_FALSE);
+                    case 'o':
+                        return check_keyword(scanner, 2, 1, "r", TOKEN_FOR);
+                    case 'u':
+                        return check_keyword(scanner, 2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i':
+            return check_keyword(scanner, 1, 1, "f", TOKEN_IF);
+        case 'n':
+            return check_keyword(scanner, 1, 2, "il", TOKEN_NIL);
+        case 'o':
+            return check_keyword(scanner, 1, 1, "r", TOKEN_OR);
+        case 'p':
+            return check_keyword(scanner, 1, 4, "rint", TOKEN_PRINT);
+        case 'r':
+            return check_keyword(scanner, 1, 5, "eturn", TOKEN_RETURN);
+        case 's':
+            return check_keyword(scanner, 1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if (scanner->current - scanner->start > 1) {
+                switch (scanner->start[1]) {
+                    case 'h':
+                        return check_keyword(scanner, 2, 2, "is", TOKEN_THIS);
+                    case 'r':
+                        return check_keyword(scanner, 2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v':
+            return check_keyword(scanner, 1, 2, "ar", TOKEN_VAR);
+        case 'w':
+            return check_keyword(scanner, 1, 4, "hile", TOKEN_WHILE);
+    }
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType check_keyword(Scanner *scanner, int start, int length,
+                               const char *rest, TokenType type)
+{
+    if (scanner->current - scanner->start == start + length &&
+        memcmp(scanner->start + start, rest, length) == 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
 }
 
 static char peek(Scanner *scanner) { return *scanner->current; }
